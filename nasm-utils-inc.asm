306b1744ba32fb505168f84c476685a98c8988f6
tdowns <travis.downs@gmail.com>
2018-01-14 16:14:05 -0600

    add multi-byte nops, conditional compilation for yasm

--- nasm-utils-inc.asm
+++ nasm-utils-inc.asm
@@ -1,6 +1,19 @@
 ;; potentially useful macros for asm development
 
-;; push the 6 callee-saved registers defined in the the sysv C ABI
+;; long-nop instructions: nopX inserts a nop of X bytes
+;; see "Table 4-12. Recommended Multi-Byte Sequence of NOP Instruction" in
+;; "Intel® 64 and IA-32 Architectures Software Developer’s Manual" (325383-061US)
+%define nop1 nop                                                     ; just a nop, included for completeness
+%define nop2 db 0x66, 0x90                                           ; 66 NOP
+%define nop3 db 0x0F, 0x1F, 0x00                                     ;    NOP DWORD ptr [EAX]
+%define nop4 db 0x0F, 0x1F, 0x40, 0x00                               ;    NOP DWORD ptr [EAX + 00H]
+%define nop5 db 0x0F, 0x1F, 0x44, 0x00, 0x00                         ;    NOP DWORD ptr [EAX + EAX*1 + 00H]
+%define nop6 db 0x66, 0x0F, 0x1F, 0x44, 0x00, 0x00                   ; 66 NOP DWORD ptr [EAX + EAX*1 + 00H]
+%define nop7 db 0x0F, 0x1F, 0x80, 0x00, 0x00, 0x00, 0x00             ;    NOP DWORD ptr [EAX + 00000000H]
+%define nop8 db 0x0F, 0x1F, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00       ;    NOP DWORD ptr [EAX + EAX*1 + 00000000H]
+%define nop9 db 0x66, 0x0F, 0x1F, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00 ; 66 NOP DWORD ptr [EAX + EAX*1 + 00000000H]
+
+;; push the 6 callee-saved registers defined in the the SysV C ABI
 %macro push_callee_saved 0
 push rbp
 push rbx
@@ -100,8 +113,13 @@ ret
 ; here we store strings needed by the failure cases, in the .rodata section
 [section .rodata]
 %1_thunk_fn_name:
+%ifdef __YASM_MAJOR__
+; yasm doesn't support defstr, so for now just use an unknown name
+db "unknown (see yasm issue #95)",0
+%else
 %defstr fname %1
 db fname,0
+%endif
 
 ; restore the previous section
 __SECT__
